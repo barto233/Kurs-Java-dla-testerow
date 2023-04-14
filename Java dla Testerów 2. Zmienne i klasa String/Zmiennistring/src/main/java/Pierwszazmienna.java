16e9c6a01ca46c34359be16e4c91c3fd5f148641
BartekRykowski <124817579+barto233@users.noreply.github.com>
2023-04-14 15:44:43 +0200

    Add files via upload

---
+++ Java dla Testerów 2. Zmienne i klasa String/Zmiennistring/src/main/java/Pierwszazmienna.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+public class Pierwszazmienna{
+
+@Test
+    public void Examples() {
+        byte number;
+        number = 2;
+        // można dokonać deklaracji i inicjalizacji w 1 lini, poniżej przykład:
+        int number2 = 8;
+        // poniżej przykład samej deklaracji (nie zwraca żadnej wartości, program sie nie wykona):
+        //int noValue;
+
+        //poniżej przykłady deklaracji pozostałych typów zmiennych
+        long longNumber = 1234567;
+        float floatNumber = 123.678F;
+        double doubleNumber = 99.23;
+        boolean isTrue = false;
+
+        char letterA = 'a';
+
+        //Deklaracja i inicjalizacja dłuższego tekstu w zmiennej typu String
+    String tekst = "Jakiś tekst";
+    System.out.println(number);
+    System.out.println(number2);
+    System.out.println(floatNumber);
+
+    }
+}
